4b49b44af91e401cf6925a933fae526f84c5629f
Tawanda M. Nyoni <tmnyoni@outlook.com>
2021-08-13 01:21:09 +0200

    added versioning header

--- main.cpp
+++ main.cpp
@@ -10,32 +10,28 @@
 
 #include "gui/main_ui.h"
 #include "appstate/app_state.h"
+#include "version_info/version_info.h"
 
 #pragma comment(linker, "/ENTRY:mainCRTStartup")
+#include <vector>
+
 
 using namespace liblec::lecui;
 
-int main(){
-	//bool is_logged_in = false;
-	//login login_window("FuelMan", is_logged_in);
-
-	//// Todo: fix the is_login issue.
+int main() {
 	std::string error;
-	state _state;
+	state app_state;
 
-	// Connect to the database.
-	if (!_state.get_db().connect(error))
+	// Creating main window.
+	main_window main_wind(appname, app_state);
+
+	if (!app_state.get_db().connect(error))
 		return 1;
-	
-	//if (login_window.show(error))
-	//	if (!is_logged_in) {
-	//		login_window.message("signin successful");
-			main_window dashboard_page("FuelMan", _state);
-			if (!dashboard_page.show(error))
-				dashboard_page.message("Error: " + error);
 
-	//	}
-	//	else
-	//		login_window.message("Error: " + error);
+	
+	if (!main_wind.show(error))
+		main_wind.message("Error: " + error);
+
+
 	return 0;
-}                                                                                                    
+}
