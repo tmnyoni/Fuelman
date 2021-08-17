3f35086229ee82705e08b33105fe80a2e83a421d
Tawanda M. Nyoni <tmnyoni@outlook.com>
2021-08-17 20:30:48 +0200

    added resources

---
+++ resource.h
@@ -0,0 +1,44 @@
+/*
+** MIT License
+**
+** Copyright(c) 2021 Tawanda M. Nyoni
+**
+** Permission is hereby granted, free of charge, to any person obtaining a copy
+** of this software and associated documentation files(the "Software"), to deal
+** in the Software without restriction, including without limitation the rights
+** to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+** copies of the Software, and to permit persons to whom the Software is
+** furnished to do so, subject to the following conditions :
+**
+** The above copyright noticeand this permission notice shall be included in all
+** copies or substantial portions of the Software.
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+** SOFTWARE.
+*/
+
+#pragma once
+
+#define add_coupon			106
+#define dispatch_coupon		107
+#define delete_coupon		108
+#define return_coupon		109
+
+#define preview_report		110
+#define print_report		111
+#define share_report		112
+
+#define ico_resource		113
+
+#define splash_image_128	114
+#define splash_image_256	115
+#define icon_png_512		116
+#define icon_png_25			117
+#define icon_png_128		118
+#define icon_png_64			119
+#define icon_png_32			120
