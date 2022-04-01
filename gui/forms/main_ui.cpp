f8a8e6c744fe755943bcb93475700ba4e2ba77aa
Tawanda M. Nyoni <tmnyoni@outlook.com>
2022-04-01 15:01:19 +0200

    Revert "added settings for updating."
    This reverts commit 9e4cda8572567ccfbb505e1f7f6b7cb670f506c1.

--- gui/forms/main_ui.cpp
+++ gui/forms/main_ui.cpp
@@ -65,7 +65,7 @@ using snap_type = lecui::rect::snap_type;
 // initialise form attributes.
 bool main_window::on_initialize(std::string& error) {
 
-	if (!_cleanup_mode || !_update_mode)
+	if (!_cleanup_mode)
 		get_dpi_scale () < 2.f 
 			? _splash_screen.display(splash_image_128, false, error) 
 			: _splash_screen.display(splash_image_256, false, error);
@@ -85,164 +85,7 @@ bool main_window::on_initialize(std::string& error) {
 		close();
 		return true;
 	}
-	else {
-		std::string value;
-		// check if there is an update ready to be installed
-		if (_settings.read_value("updates", "readytoinstall", value, error)) {}
 
-		// clear the registry entry
-		if (!_settings.delete_value("updates", "readytoinstall", error)) {}
-
-		if (!value.empty()) {
-			try {
-				const std::string fullpath(value);
-				std::filesystem::path file_path(fullpath);
-
-				const std::string directory = file_path.parent_path().string();
-				const std::string filename = file_path.filename().string();
-
-				// assume the zip file extracts to a directory with the same name
-				std::string unzipped_folder;
-				const auto idx = filename.find(".zip");
-
-				if (idx != std::string::npos)
-					unzipped_folder = directory + "\\" + filename.substr(0, idx);
-
-				// unzip the file into the same directory as the zip file
-				leccore::unzip unzip;
-				unzip.start(fullpath, directory);
-
-				while (unzip.unzipping()) {
-					if (!keep_alive()) {
-						// to-do: implement stopping mechanism
-						//unzip.stop()
-						close();
-						return true;
-					}
-				}
-
-				leccore::unzip::unzip_log log;
-				if (unzip.result(log, error) && std::filesystem::exists(unzipped_folder)) {
-					// get target directory
-					std::string target_directory;
-
-#ifdef _WIN64
-					target_directory = _install_location_64_;
-#else
-					target_directory = _install_location_32;
-#endif
-
-					if (!target_directory.empty()) {
-						if (_settings.write_value("updates", "rawfiles", unzipped_folder, error) &&
-							_settings.write_value("updates", "target", target_directory, error)) {
-
-							// run downloaded app from the unzipped folder
-#ifdef _WIN64
-							const std::string new_exe_fullpath = unzipped_folder + "\\pc_info64.exe";
-#else
-							const std::string new_exe_fullpath = unzipped_folder + "\\pc_info32.exe";
-#endif
-							if (leccore::shell::create_process(new_exe_fullpath, { "/update" }, error)) {
-								close();
-								return true;
-							}
-						}
-					}
-
-					// continue app execution normally
-				}
-				else {
-					// delete the update folder ... there many be something wrong with the update file
-					if (!leccore::file::remove_directory(directory, error)) {}
-
-					// continue app execution normally
-				}
-			}
-			catch (const std::exception&) {
-				// continue app execution normally
-			}
-		}
-		else
-			if (_update_mode) {
-				// get the location of the raw files
-				if (_settings.read_value("updates", "rawfiles", value, error) && !value.empty()) {
-					const std::string raw_files_directory(value);
-
-					if (_settings.read_value("updates", "target", value, error)) {
-						std::string target(value);
-						if (!target.empty()) {
-							try {
-								// overrwrite the files in target with the files in raw_files_directory
-								for (const auto& path : std::filesystem::directory_iterator(raw_files_directory)) {
-									std::filesystem::path p(path);
-									const std::string dest_file = target + p.filename().string();
-									std::filesystem::copy_file(path, dest_file, std::filesystem::copy_options::overwrite_existing);
-								}
-
-								// files copied successfully, now execute the app in the target directory
-#ifdef _WIN64
-								const std::string updated_exe_fullpath = target + "\\pc_info64.exe";
-#else
-								const std::string updated_exe_fullpath = target + "\\pc_info32.exe";
-#endif
-								if (leccore::shell::create_process(updated_exe_fullpath, { "/recentupdate" }, error)) {}
-
-								close();
-								return true;
-							}
-							catch (const std::exception& e) {
-								error = e.what();
-
-								// exit
-								close();
-								return true;
-							}
-						}
-					}
-				}
-				else {
-					close();
-					return true;
-				}
-			}
-			else
-				if (_recent_update_mode) {
-					// check if the updates_rawfiles and updates_target settings are set, and eliminated them if so then notify user of successful update
-					std::string updates_rawfiles;
-					if (!_settings.read_value("updates", "rawfiles", updates_rawfiles, error) && !value.empty()) {}
-
-					std::string updates_target;
-					if (!_settings.read_value("updates", "target", updates_target, error)) {}
-
-					if (!updates_rawfiles.empty() || !updates_target.empty()) {
-						if (!_settings.delete_value("updates", "rawfiles", error)) {}
-						if (!_settings.delete_value("updates", "target", error)) {}
-
-							// update inno setup version number
-							leccore::registry reg(leccore::registry::scope::current_user);
-#ifdef _WIN64
-							if (!reg.do_write("Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\" + _install_guid_64 + "_is1",
-								"DisplayVersion", std::string(appversion), error)) {
-							}
-#else
-							if (!reg.do_write("Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\" + _install_guid_32 + "_is1",
-								"DisplayVersion", std::string(appversion), error)) {
-							}
-#endif
-
-						// to-do: use a timer instead for all the calls below, for better user experience
-						message("App updated successfully to version " + std::string(appversion));
-
-						std::string updates_tempdirectory;
-						if (!_settings.read_value("updates", "tempdirectory", updates_tempdirectory, error)) {}
-						else {
-							// delete updates temp directory
-							if (!leccore::file::remove_directory(updates_tempdirectory, error)) {}
-						}
-						if (!_settings.delete_value("updates", "tempdirectory", error)) {}
-					}
-				}
-	}
 
 	std::string value;
 	if (!_settings.read_value("", "darktheme", value, error))
@@ -845,16 +688,13 @@ bool main_window::on_delete_coupon(std::string& error) {
 main_window::main_window(const std::string& caption,
 	state& app_state) :
 	_cleanup_mode(leccore::commandline_arguments::contains("/cleanup")),
-	_update_mode(leccore::commandline_arguments::contains("/update")),
-	_recent_update_mode(leccore::commandline_arguments::contains("/recentupdate")),
 	lecui::form(caption),
 	_state(app_state),
-	_settings(_registry_settings) 
-{
+	_settings(_registry_settings) {
 	
 	_registry_settings.set_registry_path("Software\\com.github.tmnyoni\\" + std::string(appname));
 
 
-	if ( _cleanup_mode || _update_mode || _recent_update_mode )
+	if (_cleanup_mode)
 		force_instance();
 }
